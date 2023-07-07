93374a7a704a2daa05ccab384f31be06c132eec2
krutko77 <70036691+krutko77@users.noreply.github.com>
2023-07-07 20:23:15 +0300

    spollers corrected

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,61 +1,7 @@
-
-// очистка полей формы
-
-// const form = document.getElementById("form");
-
-// form.addEventListener('submit', (e) => { 
-//    // Отключаем событие по умолчанию 
-//        e.prevent.Default(); 
-//    // Очищаем поля формы 
-//        e.target.reset(); 
-//    });
-
-
-
-// let button = document.querySelector('.clear-button'); 
-
-// button.addEventListener("click", submitForm);
-
-// 	function submitForm() {
-//       document.getElementById('name').value='';
-//       document.getElementById('email').value='';
-//       document.getElementById('message').value='';
-//    }
-		
-	
-// function fun() {
-//    document.getElementById('name').value='';
-//    document.getElementById('mail').value='';
-//    document.getElementById('message').value='';
-//    document.getElementById("form").reset();
-//    }
-
-
-
-
-// function testSubmit() { 
-//    var x = document.forms["myForm"]["input1"]; 
-//    var y = document.forms["myForm"]["input2"]; 
-   
-//    if (x.value === "") { 
-//       alert('plz fill!!'); 
-//       return false; 
-//       } 
-
-//    if(y.value === "") { 
-//       alert('plz fill the!!'); 
-//       return false; 
-//    } 
-   
-//    return true; 
-// } 
-         
-// function submitForm() {
-//       if (testSubmit()) { 
-//       document.getElementById("form").submit();//сначала отправьте 
-//       document.getElementById("form").reset(); // а затем сбросьте значения формы 
-//    } 
-// } 
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
 
 // Переход на страницу благодарности после отправки формы
 document.addEventListener( 'formSent', function( event ) {
@@ -63,12 +9,19 @@ document.addEventListener( 'formSent', function( event ) {
 }, false );
 
 // Защита формы от ботов через пустое поле
-let code = document.querySelector('#code'); // Получаем скрытый input
-  document.querySelector('.btn').onclick = function(){ // Клик по кнопке отправки
-    code.value = 'NOSPAM'; // Подставляем значение в value инпута	
-  };
+// Форма на странице
+if ( document.querySelector('.btn') ) {  // Проверяем наличие элемента на странице
+		let code = document.querySelector('#code'); // Получаем скрытый input
+		document.querySelector('.btn').onclick = function(){ // Клик по кнопке отправки
+			code.value = 'NOSPAM'; // Подставляем значение в value инпута	
+	};
+}
+  // Форма в popup
+  if ( document.querySelector('.btn-popup') ) {  // Проверяем наличие элемента на странице
+		let codePopup = document.querySelector('#code-popup'); // Получаем скрытый input
+		document.querySelector('.btn-popup').onclick = function(){ // Клик по кнопке отправки
+			codePopup.value = 'NOSPAM'; // Подставляем значение в value инпута	
+		};
+  }
 
-  let codePopup = document.querySelector('#code-popup'); // Получаем скрытый input
-  document.querySelector('.btn-popup').onclick = function(){ // Клик по кнопке отправки
-    codePopup.value = 'NOSPAM'; // Подставляем значение в value инпута	
-  };
+
